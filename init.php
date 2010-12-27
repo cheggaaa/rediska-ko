eded0b1feb0831a2e321297b15541889323fc0b7
Sergey Cherepanov <cheggaaa@gmail.com>
2010-12-27 23:42:08 +0300

    Autoload fix

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
-    require_once Kohana::find_file('vendor/rediska', 'library/Rediska.php');
+    require_once Kohana::find_file('vendor/rediska', 'library/Rediska');
     $config = Kohana::config('rediska');
     if (count($config)) {
         foreach ($config as $instanceName => $options) {
