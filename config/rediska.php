a827309ce490b9cb1bee034dfe221841b1b7dd9f
Sergey Cherepanov <cheggaaa@gmail.com>
2011-03-11 11:59:32 +0300

    Remove all default options

--- config/rediska.php
+++ config/rediska.php
@@ -3,44 +3,14 @@ return array(
     Rediska::DEFAULT_NAME => array (
         'name'         => Rediska::DEFAULT_NAME,
         'namespace'    => '',
-        'servers'      => array(
-            array(
-                'host'   => Rediska_Connection::DEFAULT_HOST,
-                'port'   => Rediska_Connection::DEFAULT_PORT,
-                'weight' => Rediska_Connection::DEFAULT_WEIGHT,
-            )
-        ),
-        'serializeradapter' => 'phpSerialize',
-        'keydistributor'    => 'consistentHashing',
-        'redisversion'      => Rediska::STABLE_REDIS_VERSION,
     ),
     'cache'   => array (
         'name'         => 'cache',
         'namespace'    => 'cache::',
-        'servers'      => array(
-            array(
-                'host'   => Rediska_Connection::DEFAULT_HOST,
-                'port'   => Rediska_Connection::DEFAULT_PORT,
-                'weight' => Rediska_Connection::DEFAULT_WEIGHT,
-            )
-        ),
-        'serializeradapter' => 'phpSerialize',
-        'keydistributor'    => 'consistentHashing',
-        'redisversion'      => Rediska::STABLE_REDIS_VERSION,
     ),
     'session'  => array (
         'name'         => 'session',
         'namespace'    => 'session::',
-        'servers'      => array(
-            array(
-                'host'   => Rediska_Connection::DEFAULT_HOST,
-                'port'   => Rediska_Connection::DEFAULT_PORT,
-                'weight' => Rediska_Connection::DEFAULT_WEIGHT,
-            )
-        ),
-        'serializeradapter' => 'phpSerialize',
-        'keydistributor'    => 'consistentHashing',
-        'redisversion'      => Rediska::STABLE_REDIS_VERSION,
     )
 );
 
